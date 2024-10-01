e96333b989a0befded1aa93196f588347ec1be3b
Ponleur7373 <yeanponleur73@gmail.com>
2024-10-01 15:19:25 +0700

    updated code

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("ponleur")
+def add(a,b):
+    if a >0 :
+        return a+b
+    else:
+        return "Enter values that big that sero"
+a = int(input("Enter a :"))
+b = int(input("Enter b :"))
+result= add(a,b)
+print(result)
